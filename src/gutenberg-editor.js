69aa1dc020e06026fe914545df42d0600bdcd134
Marko Andrijasevic <markoandrijasevic@gmail.com>
2018-09-05 04:05:36 +0200

    Add Gutenberg visual editor

---
+++ src/gutenberg-editor.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import apiFetch from '@wordpress/api-fetch';
+import { dispatch } from '@wordpress/data';
+import '@wordpress/core-data'; // Initializes core data store
+import { registerCoreBlocks } from '@wordpress/block-library';
+import {
+	BlockList,
+	CopyHandler,
+	PostTitle,
+	WritingFlow,
+	ObserveTyping,
+	EditorGlobalKeyboardShortcuts,
+	EditorProvider,
+	ErrorBoundary,
+	BlockSelectionClearer,
+	MultiSelectScrollIntoView,
+} from '@wordpress/editor';
+
+const editorSettings = {};
+
+const post = {
+	type: 'post',
+	content: 'test content',
+};
+
+registerCoreBlocks();
+
+// NO API support
+apiFetch.use( () => {} );
+
+dispatch( 'core/nux' ).disableTips();
+
+class GutenbergEditor extends Component {
+	render() {
+		return (
+            <EditorProvider settings={ editorSettings } post={ post }>
+                <ErrorBoundary onError={ () => {} }>
+                    <BlockSelectionClearer>
+                        <EditorGlobalKeyboardShortcuts />
+                        <CopyHandler />
+                        <MultiSelectScrollIntoView />
+                        <WritingFlow>
+	                        <ObserveTyping>
+		                        <PostTitle />
+		                        <BlockList />
+	                        </ObserveTyping>
+                        </WritingFlow>
+                    </BlockSelectionClearer>
+                </ErrorBoundary>
+            </EditorProvider>
+		);
+	}
+}
+
+export default GutenbergEditor;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './style.css';
+import GutenbergEditor from './gutenberg-editor';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render( <GutenbergEditor />, document.getElementById( 'root' ) );
